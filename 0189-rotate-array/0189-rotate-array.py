575c20b4a0ccf1a3bd7e684ccc3154402f7beef7
curiosity-matrix <77ritikasingh77@gmail.com>
2026-06-23 14:50:28 +0530

    Time: 7 ms (53.38%), Space: 23.2 MB (32.37%) - LeetHub - Time: O(n), Space: O(1)

---
+++ 0189-rotate-array/0189-rotate-array.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def reverse(self, nums, l, r):
+        while l < r:
+            nums[l], nums[r] = nums[r], nums[l]
+            l += 1
+            r -= 1
+
+    def rotate(self, nums, k):
+        n = len(nums)
+        k = k % n
+
+        self.reverse(nums, 0, n - 1)
+        self.reverse(nums, 0, k - 1)
+        self.reverse(nums, k, n - 1)
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
