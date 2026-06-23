00cbeed994a585da877caeebfcdda0678a1a1105
curiosity-matrix <77ritikasingh77@gmail.com>
2026-06-23 14:48:25 +0530

    Time: 0 ms (100%), Space: 12.5 MB (18.5%) - LeetHub - Time: O(n log n), Space: O(n)

---
+++ 0075-sort-colors/0075-sort-colors.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def sortColors(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: None Do not return anything, modify nums in-place instead.
+        """
+        
+        nums.sort()
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
