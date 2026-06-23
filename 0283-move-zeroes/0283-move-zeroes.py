e84fdc2e67867a15fea544312f625a68669b3f43
curiosity-matrix <77ritikasingh77@gmail.com>
2026-06-23 14:48:41 +0530

    Time: 0 ms (100%), Space: 13.6 MB (19.44%) - LeetHub - Time: O(n), Space: O(1)

---
+++ 0283-move-zeroes/0283-move-zeroes.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def moveZeroes(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: None Do not return anything, modify nums in-place instead.
+        """
+        i = 0
+
+        for j in range(len(nums)):
+            if nums[j] != 0:
+                nums[i] = nums[j]
+                i += 1
+
+        for k in range(i, len(nums)):
+            nums[k] = 0
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
