2829c8d5d48c6bc96ad52d66869cd646128cbfda
curiosity-matrix <77ritikasingh77@gmail.com>
2026-07-30 08:13:17 +0530

    Time: 19 ms (64.65%), Space: 12.6 MB (50.37%) - LeetHub - Time: O(n + m), Space: O(1)

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -5,7 +5,21 @@ class Solution(object):
         :type t: str
         :rtype: bool
         """
+        """
         return sorted(s)==sorted(t)
+        """
+        arr1=[0]*26
+        arr2=[0]*26
+        for ch1 in s:
+            idx1=ord(ch1)-ord('a')
+            arr1[idx1]+=1
+        for ch2 in t:
+            idx2=ord(ch2)-ord('a')
+            arr2[idx2]+=1
+        return arr1==arr2
+            
+        return arr1==arr2
+
 
 # Synced seamlessly with LeetHub Pro
 # Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
